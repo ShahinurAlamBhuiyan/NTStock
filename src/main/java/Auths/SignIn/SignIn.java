3e5dc09b57ed65d62c5156b3fc214f2f3ec85bbe
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2022-11-28 20:04:49 +0600

    [merge] with shahin

--- src/main/java/Auths/SignIn/SignIn.java
+++ src/main/java/Auths/SignIn/SignIn.java
@@ -1,20 +1,7 @@
 package Auths.SignIn;
 
 public class SignIn{
-//
-//    @Override
-//    public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(Auths.SignIn.class.getResource("Auths.SignIn.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load());
-//        stage.setTitle("NTStock");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//    // merge with
-//
-//    public static void main(String[] args) {
-//        launch();
-//    }
+    // merge with shahin branch
 }
 
 
