1242f423b5ca08e4c67202dd9970da309c8be481
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2022-12-06 00:11:59 +0600

    [merge] with main

--- src/main/java/Slides/SlideOne/SlideOne.java
+++ src/main/java/Slides/SlideOne/SlideOne.java
@@ -24,7 +24,7 @@ public class SlideOne extends Application {
 
         stage.show();
     }
-    // merge with all branch
+    // merge with all main
     public static void main(String[] args) {
         launch();
     }
