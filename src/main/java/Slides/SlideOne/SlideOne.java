887cb4792496fc43338181b74adc838a9e9af883
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2022-12-09 20:19:19 +0600

    [merge] with _Nafis

--- src/main/java/Slides/SlideOne/SlideOne.java
+++ src/main/java/Slides/SlideOne/SlideOne.java
@@ -27,7 +27,7 @@ public class SlideOne extends Application {
 
         stage.show();
     }
-    // merge with  main
+    // merge with  nafis
     public static void main(String[] args) {
         launch();
     }
